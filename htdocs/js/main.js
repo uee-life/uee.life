66745aca61f140d75329efac894d4aacb29ef8a4
Capn Flint <capnflinttv@gmail.com>
2019-10-06 18:11:23 -0700

    lkj

--- htdocs/js/main.js
+++ htdocs/js/main.js
@@ -1,5 +1,90 @@
 var apiURL = 'https://www.capnflint.com:4443'
 
+Vue.component('planet', {
+    props: {
+        test: {
+            type: String,
+            required: true,
+            default: "Daymar"
+        }
+    },
+    template: `
+        <div>
+            <div class="planet-info">
+                <h3>Planet: {{ test }}</h3>
+            </div>
+            <div id="map" class="map">
+                <x3d id="x3d" width='600px' height='600px'> 
+                    <scene> 
+                        <navigationInfo type="none"></navigationInfo>
+                        <viewpoint id="start" position="0 140 1150" orientation="-1 0 0 0.104"></viewpoint>
+                        <directionalLight id="directional" direction='-1 -0.4 -0.6' on ="TRUE" intensity='4.0' shadowIntensity='0.1'></directionalLight>
+                        <matrixTransform id="teapotRotation">
+                            <group>
+                                <!-- The main globe -->
+                                <cylinderSensor autoOffset='false' axisRotation='0 1 0 -1.57' onoutputchange='processRotationGizmoEvent(event);'></cylinderSensor>
+            
+                                <transform id="globe" translation="0 0 0">
+                                    <shape>
+                                        <appearance>
+                                            <ImageTexture class="texture" :url="texture"></ImageTexture>
+                                        </appearance>
+                                        <sphere class="radius" :radius="radius"></sphere>
+                                    </shape>
+                                </transform>
+                            </group>
+                            <transform id="markers" translation="0 0 0">
+        
+                            </transform>
+        
+                            <transform id="POIs" translation="0 0 0">
+        
+                            </transform>
+                        </matrixTransform>
+                    </scene>
+                </x3d> 
+            </div>
+            <ul class="info">
+                <li v-for="(detail, key) in details">{{ key }}: {{ detail }}</li>
+            </ul>
+        </div>
+    `,
+    data() {
+        return {
+            xname: 'Daymar',
+            link: 'https://www.google.com',
+            details: {
+                Name: "Daymar",
+                Owner: "UEE",
+                Type: "Planet"
+            },
+            radius: 295
+        }
+    },
+    methods: {
+        getData: function(location) {
+            var xhr = new XMLHttpRequest()
+            var self = this
+            var data = {
+                location: location
+            }
+            xhr.open('POST', apiURL + '/stargps/location')
+            xhr.onload = function() {
+                self.data = JSON.parse(xhr.responseText)
+                console.log(self.data)
+            }
+        },
+        loadLocation(data) {
+
+        }
+    },
+    computed: {
+        texture() {
+            return './images/textures/' + this.test + '.jpg'
+        }
+    }
+});
+
 Vue.component('org', {
     template: `
         <div class="org">
@@ -29,6 +114,6 @@ Vue.component('org', {
 var app = new Vue({
     el: '#app',
     data: {
-        test: 'Daymar'
+        test: 'Hurston'
     }
 });
