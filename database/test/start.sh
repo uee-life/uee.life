efe16ae1454e46faa4542cc643cafe0cb16ab8c9
Capn Flint <capnflinttv@gmail.com>
2019-12-01 14:57:39 -0800

    Update start.sh

--- database/test/start.sh
+++ database/test/start.sh
@@ -1,2 +1,2 @@
 
-sudo docker run --name ueelife-db -v -e MYSQL_ROOT_PASSWORD_FILE=mysql_root_pass_config.txt --net ueelife mariadb
+sudo docker run --name ueelife-db -v -e MYSQL_ROOT_PASSWORD_FILE=mysql_root_pass_config.txt --net ueelife ueelife-db
