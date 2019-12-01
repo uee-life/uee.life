644d71d3960d214a6a0f749e411f008e5e9064c8
Capn Flint <capnflinttv@gmail.com>
2019-12-01 13:21:27 -0800

    Update index.js

--- api/src/index.js
+++ api/src/index.js
@@ -21,18 +21,7 @@ const app = express();
 app.use(helmet());
 app.use(bodyParser.json());
 
-var whitelist = ['https://uee.life', 'https://www.uee.life', 'http://localhost:8080']
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
-
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(morgan('combined'));
 
