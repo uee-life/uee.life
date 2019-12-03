2eb594bb8e1033c22209de31f46aaf31f1868225
Capn Flint <capnflinttv@gmail.com>
2019-12-03 10:38:32 -0800

    Update LatestCitizen.vue

--- app/src/components/main/LatestCitizen.vue
+++ app/src/components/main/LatestCitizen.vue
@@ -20,7 +20,7 @@ export default {
     name: "latest-citizen",
     data() {
         return {
-          latest: "anadiashark",
+          latest: "FrenchFriedDakoter",
             citizen: null
         }
     },
