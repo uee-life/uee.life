9e1658c0e26a05992ed5a470927652b1f2d7b6b0
Capn Flint <capnflinttv@gmail.com>
2019-12-07 23:19:36 -0800

    location pages

---
+++ app/src/components/location/Location.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="location">
+        <main-panel>
+            <h3> {{ location.name }} {{ type }} ( {{ location.affiliation }} )</h3>
+            <p> {{ location.description }} </p>
+        </main-panel>
+        <div v-if="debug">{{ JSON.stringify(location, null, 2) }}</div>
+        <!--div class="org-tabs">
+            <tabs :tabs="tabs" :initialTab="initialTab">
+                <template slot="tab-title-info">
+                    INFO
+                </template>
+                <template slot="tab-content-info">
+                    <org-info :org="org" />
+                </template>
+
+                <template slot="tab-title-fleet">
+                    FLEET
+                </template>
+                <template slot="tab-content-fleet">
+                    <fleet-view :ships="fleet"/>
+                </template>
+
+                <template slot="tab-title-members">
+                    MEMBERS
+                </template>
+                <template slot="tab-content-members">
+                    <org-members :members="members" />
+                </template>
+            </tabs>
+        </div-->
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'location',
+    props: ['location', 'type'],
+    data() {
+        return {
+            tabs: ["info", "fleet", "members"],
+            initialTab: "info",
+            debug: false
+        }
+    },
+    components: {
+    },
+    computed: {
+        mainTitle() {
+            return `${this.location.name} ${this.type} ( ${this.location.affiliation} )`
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .location {
+        width: 100%;
+        padding: 10px;
+        padding-top: 20px;
+    }
+</style>
