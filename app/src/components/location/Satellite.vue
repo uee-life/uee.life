88025f0e315b068277eaee1dcaaf368e0bfedf2a
Capn Flint <capnflinttv@gmail.com>
2019-12-08 20:50:59 -0800

    Update Satellite.vue

--- app/src/components/location/Satellite.vue
+++ app/src/components/location/Satellite.vue
@@ -2,9 +2,9 @@
     <div class="satellite">
         <left-dock />
         <portal to="navigationPane">
-            <div v-if="satellite" class="left-nav-button"><router-link :to="systemLink">{{satellite.system}} system</router-link></div>
-            <div v-if="satellite" class="left-nav-button"><router-link :to="planetLink">{{satellite.planet}} system</router-link></div>
-            <div class="left-nav-button"><a target="_blank" :href="starmapLink">Starmap Link</a></div>
+            <div v-if="satellite" class="left-nav-button"><router-link :to="systemLink">{{satellite.system}} System</router-link></div>
+            <div v-if="satellite" class="left-nav-button"><router-link :to="planetLink">Planet {{satellite.planet}}</router-link></div>
+            <div class="left-nav-button"><a target="_blank" :href="starmapLink">Open in Starmap</a></div>
         </portal>
         <location :location="satellite" type="Satellite">
         </location>
@@ -46,7 +46,7 @@ export default {
     },
     computed: {
         starmapLink() {
-            if(this.planet) {
+            if(this.satellite) {
                 return `https://robertsspaceindustries.com/starmap?location=${this.satellite.code}&system=${this.satellite.system}`
             } else {
                 return ""
