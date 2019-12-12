84f1725f8c1cf57e55cde947726bd5a09657653a
Capn Flint <capnflinttv@gmail.com>
2019-12-11 18:02:36 -0800

    Update location.js

--- api/src/db/location.js
+++ api/src/db/location.js
@@ -2,7 +2,7 @@
 const {executeSQL} = require('./mariadb')
 
 async function getSystems() {
-    return await executeSQL("SELECT id, name FROM systems WHERE affiliation IN (1, 5, 6)")
+    return await executeSQL("SELECT a.id, a.name, b.affiliation FROM systems a LEFT JOIN affiliation b ON a.affiliation = b.id;")
 }
 
 async function getSystem(sys) {
