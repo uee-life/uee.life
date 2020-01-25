1acda9e6b27c2a2b56f41594b9d92178294d6259
Capn Flint <capnflinttv@gmail.com>
2020-01-25 14:48:30 -0800

    Update ships.js

--- api/src/db/ships.js
+++ api/src/db/ships.js
@@ -1,8 +1,15 @@
 const axios = require("axios")
 
 async function syncShips() {
-    
-    return {success: "Success!"}
+    let result = axios({
+        url: 'https://calculator-api-259617.appspot.com/mongoDocuments/ships',
+        method: 'GET'
+    }).then((res) => {
+        return res.data.length()
+    }).catch((err) => {
+        console.error(err)
+    })
+    return {success: "Success!", result: result}
 }
 
 module.exports = {
