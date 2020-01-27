44725f3e4a823880c9b3c31ece56a278d6725a30
Capn Flint <capnflinttv@gmail.com>
2020-01-27 15:44:44 -0800

    Update organization.js

--- api/src/db/organization.js
+++ api/src/db/organization.js
@@ -156,7 +156,7 @@ function getMarkdown($, chunk) {
 async function test() {
     const html = '<p>Page Under Construction. Please check back soon!<br />test</p>' +
 				'<h2>H2</h2><h3>H3</h3><h4>H4</h4><h5>H5</h5><h6>H6</h6><p>Paragraph</p><p><strong>bold</strong></p>' +
-                '<p><del>strike</del></p><ul><li>bullet</li></ul><ol><li>number</li></ol>' +
+                '<p><del>strike</del></p><ul><li>bullet</li><li>bullet 2</li></ul><ol><li>number</li></ol>' +
                 '<p><a href="http://uee.life" rel="nofollow" title="link title">link</a></p><blockquote>' +
                 '<p>quote</p></blockquote><p><code>code</code></p>'
  
@@ -194,7 +194,13 @@ async function test() {
     })
     $('ul').each(function(i, el) {
         $(el).find('li').each(function(i, el) {
-            $(el).replaceWith('* ' + $(el).text())
+            $(el).replaceWith('* ' + $(el).text() + '\n')
+        })
+        $(el).replaceWith($(el).text() + '\n\n')
+    })
+    $('ol').each(function(i, el) {
+        $(el).find('li').each(function(i, el) {
+            $(el).replaceWith('# ' + $(el).text())
         })
     })
 
