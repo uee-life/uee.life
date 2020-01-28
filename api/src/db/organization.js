e7d15e251a5fc6d51a6c6d9f8a8120b480a3d70f
Capn Flint <capnflinttv@gmail.com>
2020-01-27 23:30:52 -0800

    moving convert to markdown into a util module

--- api/src/db/organization.js
+++ api/src/db/organization.js
@@ -1,5 +1,6 @@
 const axios = require('axios')
 const cheerio = require('cheerio')
+const { convertToMarkdown } = require('./utils')
 
 async function fetchMembers(org, page, isMain) {
     let members = {
@@ -131,70 +132,6 @@ async function fetchOrg(org) {
     }
 }
 
-tagmap = {
-
-}
-
-async function convertToMarkdown(html) {
-    const $ = cheerio.load(html)
-
-    $('h1').each(function (i, el) {
-        $(el).replaceWith('# ' + $(el).text() + '\n\n')
-    })
-    $('h2').each(function (i, el) {
-        $(el).replaceWith('## ' + $(el).text() + '\n\n')
-    })
-    $('h3').each(function (i, el) {
-        $(el).replaceWith('### ' + $(el).text() + '\n\n')
-    })
-    $('h4').each(function (i, el) {
-        $(el).replaceWith('#### ' + $(el).text() + '\n\n')
-    })
-    $('h5').each(function (i, el) {
-        $(el).replaceWith('##### ' + $(el).text() + '\n\n')
-    })
-    $('h6').each(function (i, el) {
-        $(el).replaceWith('###### ' + $(el).text() + '\n\n')
-    })
-    $('strong').each(function (i, el) {
-        $(el).replaceWith('**' + $(el).text() + '**')
-    })
-    $('del').each(function (i, el) {
-        $(el).replaceWith('~~' + $(el).text() + '~~')
-    })
-    $('code').each(function (i, el) {
-        $(el).replaceWith('`' + $(el).text() + '`')
-    })
-    $('a').each(function (i, el) {
-        $(el).replaceWith('[' + $(el).text() + '](' + $(el).attr('href') + ')')
-    })
-
-    $('ul').each(function(i, el) {
-        $(el).find('li').each(function(i, el) {
-            $(el).replaceWith('* ' + $(el).text())
-        })
-        $(el).replaceWith($(el).text().trim() + '\n')
-    })
-    $('ol').each(function(i, el) {
-        $(el).find('li').each(function(i, el) {
-            $(el).replaceWith('1. ' + $(el).text())
-        })
-        $(el).replaceWith($(el).text() + '\n')
-    })
-
-    $('blockquote').each(function (i, el) {
-        $(el).find('br').each(function(i, el2) {
-            $(el2).replaceWith('\n> ')
-        })
-        $(el).replaceWith('> ' + $(el).html().replace(/(\n)[^\&]/g, ''))
-    })
-
-    /*$('p').each(function (i, el) {
-        $(el).replaceWith($(el).html())
-    })*/
-
-    return $.text().replace(/\t/g, '').replace(/\>\s+/, '> ')
-}
 
 async function test() {
     return convertToMarkdown()
