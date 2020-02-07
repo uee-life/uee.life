1be1bf7bbd0f4f6c25a14f4f86bda782b93af569
Capn Flint <capnflinttv@gmail.com>
2020-02-07 01:22:31 -0800

    Cleanup (#2)
    * clean up workspace
    
    * cleaning up old code...

--- app_old/start.sh
+++
@@ -1,2 +0,0 @@
-sudo docker build -t uee-life:prod .
-sudo docker run -d --restart unless-stopped  -p 127.0.0.1:8080:80 --name uee-life uee-life:prod
--- app_old/test.sh
+++
@@ -1,2 +0,0 @@
-sudo docker build -t uee-life:test .
-sudo docker run --rm -p 0.0.0.0:8090:80 --name uee-life-test uee-life:test
