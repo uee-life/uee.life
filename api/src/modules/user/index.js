4b8d129d5a8edcaf5741df967a5f45f9a8060023
Capn Flint <capnflint@gmail.com>
2020-02-22 14:12:33 +0000

    debugging api refactor

--- api/src/modules/user/index.js
+++ api/src/modules/user/index.js
@@ -7,10 +7,22 @@ const {
 
 router.get("/user", checkJwt, async (req, res) => {
     console.log('/user called')
+    console.log(req.user)
+    console.log('************')
     res.send(await getUser(req.headers.authorization).catch(err => {
         console.error(err)
         return err
     }));
+});
+
+// Protected
+router.get("/user/verify", checkJwt, async (req, res) => {
+    res.send(await verifyCitizen(req.headers.authorization));
+});
+
+// Protected
+router.get("/user/sync", checkJwt, async (req, res) => {
+    res.send(await startSync(req.headers.authorization))
 });
 
 module.exports = router
