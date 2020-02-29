a6c8f6cf8a3e20ca50d9fd934719095c0e3525c7
Capn Flint <capnflint@gmail.com>
2020-02-29 14:02:40 +0000

    adding prod and test build scripts

---
+++ start.prod.sh
@@ -0,0 +1,2 @@
+sudo docker-compose -p prod -f docker-compose.prod.yml build
+sudo docker-compose -p prod -f docker-compose.prod.yml up -d
---
+++ start.test.sh
@@ -0,0 +1,2 @@
+sudo docker-compose -p test -f docker-compose.test.yml build
+sudo docker-compose -p test -f docker-compose.test.yml up -d
