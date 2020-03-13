3a42c1c2f3c76d89500bc1836072fd7ac635102a
Capn Flint <capnflinttv@gmail.com>
2020-03-13 01:31:02 -0700

    flexhing out article page and video support

---
+++ app/components/layout/Youtube.vue
@@ -0,0 +1,36 @@
+<template>
+    <main-panel class="youtube" :title="title">
+        <div class="description">
+            <slot></slot>
+        </div>
+        <div class="video">
+            <iframe width="560" height="315" :src="videoLink" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+    </main-panel>
+</template>
+
+<script>
+export default {
+    name: 'video',
+    props: ['title', 'vid'],
+    computed: {
+        videoLink() {
+            return `https://www.youtube-nocookie.com/embed/${this.vid}`
+        }
+    }
+}
+</script>
+
+<style>
+.youtube {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    flex-basis: 560px;
+}
+
+.youtube .video {
+    display: flex;
+    justify-content: center;
+}
+</style>
--- app/pages/tools/index.vue
+++ app/pages/tools/index.vue
@@ -1,15 +1,22 @@
 <template>
   <div class="tools">
       <h1>Coming Soon!</h1>
-      <nuxt-link to="/ships">ships</nuxt-link>
+      <youtube title="Carrack Video" vid="5BBg3BTbcLo">
+        This is a video of a Carrack doing things...
+      </youtube>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
 
+import Youtube from '@/components/layout/Youtube.vue'
+
 export default {
-  name: 'tools'
+  name: 'tools',
+  components: {
+    Youtube
+  }
 }
 </script>
 
