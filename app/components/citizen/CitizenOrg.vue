50688d156e95cf01da7b4aacff56257c1a018b51
Capn Flint <capnflinttv@gmail.com>
2020-05-11 11:20:23 -0700

    Update CitizenOrg.vue

--- app/components/citizen/CitizenOrg.vue
+++ app/components/citizen/CitizenOrg.vue
@@ -6,7 +6,7 @@
           {{ citizen.org.name }}
         </div>
         <div class="org-title">
-          Title: {{ citizen.info.orgrank }}
+          Title: {{ citizen.info.orgTitle }}
         </div>
       </div>
   </dock-item>
